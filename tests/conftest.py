7893d11a6ac0ca4eeb60ce3ad52d237ec5c621b4
Georgii Bochorishvili <georgii_bochorishvili@epam.com>
2021-11-16 00:44:36 +0300

    mock database connection

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,35 @@
 from pytest import fixture
+from mongoengine import connect
 
+from gateways.models import User
 from gateways.mongo import Mongo
 
 
 @fixture(scope="session")
 def mongo():
     return Mongo().connection
+
+
+@fixture
+def mongo_mock():
+    return connect('mongoenginetest', host='mongomock://localhost')
+
+
+@fixture
+def users_with_phone_numbers_mock(mongo_mock):
+    user_tosin = User(
+        name='Tosin',
+        surname='Abasi',
+        phoneNumber='88005553535',
+        workPhoneNumber='88003590000'
+    )
+    user_linus = User(
+        name='Linus',
+        surname='Torvalds',
+        phoneNumber='8(800)1110000',
+        workPhoneNumber='+79991112233'
+    )
+    user_tosin.save()
+    user_linus.save()
+
+    return mongo_mock
--- tests/test_user_search.py
+++ tests/test_user_search.py
@@ -1,6 +1,6 @@
 from app import get_user_by_phone_number
 
 
-def test_search_user_by_phone_number(mongo):
+def test_search_user_by_phone_number(users_with_phone_numbers_mock):
     found_users = get_user_by_phone_number('555')
-    assert len(found_users) > 1
+    assert len(found_users) > 0
